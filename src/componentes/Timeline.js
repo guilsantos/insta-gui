332893b461313c5ed8b3b5b69720a2c05b31cc7d
guilherme.santos <guiga021@hotmail.com>
2018-03-31 11:13:27 -0300

    Aula 1

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import FotoItem from './Foto';
-import Pubsub from 'pubsub-js';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class Timeline extends Component {
@@ -8,32 +7,13 @@ export default class Timeline extends Component {
   constructor(){
     super();
     this.state = {fotos: []};
+    //this.login = this.props.login;
   }
 
   componentWillMount(){
-    Pubsub.subscribe('timeline', (topico, fotos) => {
+    this.props.store.subscribe(fotos => {
       this.setState({fotos});
     })
-
-    Pubsub.subscribe('atualiza-liker', (topico, infoLiker) => {
-      const fotoEncontrada = this.state.fotos.find(foto => foto.id === infoLiker.fotoId);
-      fotoEncontrada.likeada = !fotoEncontrada.likeada;
-
-      const possivelLiker = fotoEncontrada.likers.find(liker => liker.login === infoLiker.liker.login);
-      if(possivelLiker === undefined){
-        fotoEncontrada.likers.push(infoLiker.liker);
-      } else {
-        const novoslikers = fotoEncontrada.likers.filter(liker => liker.login !== infoLiker.liker.login);
-        fotoEncontrada.likers = novoslikers;
-      }
-      this.setState({fotos: this.state.fotos});
-    });
-
-    Pubsub.subscribe('novos-comentarios', (topico, infoComentario) => {
-      const fotoEncontrada = this.state.fotos.find(foto => foto.id === infoComentario.fotoId);
-      fotoEncontrada.comentarios.push(infoComentario.novoComentario);
-      this.setState({fotos: this.state.fotos});
-    });
   }
 
   carregarFotos(props){
@@ -45,9 +25,7 @@ export default class Timeline extends Component {
       urlPerfil = `https://instalura-api.herokuapp.com/api/public/fotos/${props.login}`
     }
 
-    fetch(urlPerfil)
-    .then(response => response.json())
-    .then(fotos => this.setState({fotos:fotos}));
+    this.props.store.lista(urlPerfil);
   }
 
   componentDidMount(){
@@ -61,39 +39,11 @@ export default class Timeline extends Component {
   }
 
   like(fotoId){
-    fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/like?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, {method:'POST'})
-    .then(response => {
-      if(response.ok){
-        return response.json();
-      } else {
-        throw new Error('Não foi possível realizar o like da foto');
-      }
-    })
-    .then(liker => {
-      Pubsub.publish('atualiza-liker', { fotoId, liker });
-    });
+    this.props.store.like(fotoId);
   }
 
   comenta(fotoId, comentario){
-    const requestInfo = {
-      method: 'POST',
-      body: JSON.stringify({ texto: comentario }),
-      headers: new Headers({
-        'Content-type': 'application/json'
-      })
-    };
-
-    fetch(`https://instalura-api.herokuapp.com/api/fotos/${fotoId}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
-    .then(response => {
-      if(response.ok){
-        return response.json();
-      } else {
-        throw new Error('não foi possivel comentar');
-      }
-    })
-    .then(novoComentario => {
-      Pubsub.publish('novos-comentarios', {fotoId, novoComentario})
-    })
+    this.props.store.comenta(fotoId, comentario);
   }
 
   render() {
@@ -104,7 +54,7 @@ export default class Timeline extends Component {
           transitionEnterTimeout={500}
           transitionLeaveTimeout={300}>
           {
-            this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like} comenta={this.comenta}/>)
+            this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like.bind(this)} comenta={this.comenta.bind(this)}/>)
           }
         </ReactCSSTransitionGroup>
       </div>
